603fd97ef0ecb000e9c465c415687008ae2dec1c
Md. Rezve Hasan <7360054+Rezve@users.noreply.github.com>
2025-04-05 00:52:43 +0600

    Remove debug log from ipc handler

--- electronjs/mssql-fake-data-generator/src/main/ipc-handlers.ts
+++ electronjs/mssql-fake-data-generator/src/main/ipc-handlers.ts
@@ -9,27 +9,22 @@ function registerHandlers(mainWindow: BrowserWindow) {
   ipcMain.handle('config:db',  async (event, dbConfig) => DataGeneratorManager.setDBConfig(event, dbConfig))
 
   ipcMain.handle('storage:retrieve-key', async (event, account) => {
-    console.log("🚀 ~ ipcMain.handle ~ storage:retrieve-key:", account)
     return await getKey(account);
   });
 
   ipcMain.handle('storage:encrypt', async (event, { text, key }) => {
-    console.log("🚀 ~ ipcMain.handle ~ storage:encrypt:", text, key)
     return await encrypt(text, key);
   });
 
   ipcMain.handle('storage:decrypt', async (event, { encryptedText, key, iv }) => {
-    console.log("🚀 ~ ipcMain.handle ~ storage:decrypt:", encryptedText, key, iv)
     return await decrypt(encryptedText, key, iv);
   });
 
   ipcMain.handle('storage:saveConfig', async (event, config) => {
-    console.log("🚀 ~ ipcMain.handle ~ storage:saveConfig:", config)
     return await saveConfig(config);
   });
 
   ipcMain.handle('storage:loadConfig', async (event) => {
-    console.log("🚀 ~ ipcMain.handle ~ storage:loadConfig:")
     return await loadConfig();
   });
 }
